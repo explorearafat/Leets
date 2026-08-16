e366faa85a8ac642fc7d6181d5186f11ed8bf34a
Eyasin Arafat <254892431+explorearafat@users.noreply.github.com>
2026-08-16 21:08:34 +0600

    Time: 288 ms (33.46%), Space: 120.3 MB (57.64%) - LeetHub

---
+++ 2630-memoize-ii/2630-memoize-ii.js
@@ -0,0 +1,23 @@
+var memoize = function(fn) {
+    const cache = new Map();
+
+    return function(...args) {
+        let current = cache;
+
+        for (const arg of args) {
+            if (!current.has(arg)) {
+                current.set(arg, new Map());
+            }
+            current = current.get(arg);
+        }
+
+        if (current.has("result")) {
+            return current.get("result");
+        }
+
+        const result = fn(...args);
+        current.set("result", result);
+
+        return result;
+    };
+};
